46ad79b64923312467682a649f0c1bae95a87850
Bruno Lima <bru.no@outlook.com.br>
2024-05-24 16:26:54 -0300

    Criação do componente que exibe o nível da umidade relativa do ar

---
+++ src/components/umidade/Umidade.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+import "./Umidade.css";
+
+const Umidade = () => {
+  const [valor, setValor] = useState(null);
+
+  const fetchUmidade = async () => {
+    try {
+      const response = await axios.get("/api/sensores/umidade");
+      setValor(response.data.valor);
+    } catch (error) {
+      console.error("Erro ao buscar umidade", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUmidade();
+    const interval = setInterval(fetchUmidade, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="umidade">
+      <span className="umidade-nome">Umidade:</span>
+
+      <div className="umidade-display">
+        <Image
+          className="umidade-icone"
+          src="/icones/umidade.png"
+          alt="Umidade"
+          width={100}
+          height={100}
+          priority={true}
+        />
+
+        {valor !== null ? (
+          <span className="umidade-dados">{valor}</span>
+        ) : (
+          <span className="umidade-dados-carregando">Carregando...</span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Umidade;
