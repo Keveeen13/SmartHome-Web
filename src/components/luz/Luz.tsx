8f78073c48b3ca2650045da0e55188252c9d4965
Blimabru <bru.no@outlook.com.br>
2024-05-29 01:16:37 -0300

    Refatoração do componente Luz

--- src/components/luz/Luz.tsx
+++ src/components/luz/Luz.tsx
@@ -1,21 +1,23 @@
-"use client";
+'use client';
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import axios from "axios";
 import "./Luz.css";
 
-const Luz = ({ dispositivo, alias }) => {
+const Luz = ({ dispositivo, ambiente, andar, alias }) => {
   const [estado, setEstado] = useState(null); // Alterado o estado inicial para null
+
+  const tipo = "Luz";
 
   const fetchEstado = async () => {
     try {
       const response = await axios.post("/api/dispositivos/estadoAtual", {
-        dispositivo,
+        dispositivo, ambiente, andar, tipo
       });
       setEstado(response.data.estado);
     } catch (error) {
-      console.error("Erro ao buscar estado inicial do " + dispositivo, error);
+      console.error("Erro ao buscar estado inicial do " + dispositivo, ambiente, andar, tipo, error);
       setEstado(null); // Define o estado como null em caso de erro
     }
   };
@@ -23,17 +25,17 @@ const Luz = ({ dispositivo, alias }) => {
   const toggleLuz = async () => {
     try {
       const response = await axios.post("/api/dispositivos/alterarEstado", {
-        dispositivo,
+        dispositivo, ambiente, andar, tipo
       });
       setEstado(response.data.estado);
     } catch (error) {
-      console.error("Erro ao alternar estado do " + dispositivo, error);
+      console.error("Erro ao alternar estado do " + dispositivo, ambiente, andar, tipo, error);
     }
   };
 
   useEffect(() => {
     fetchEstado();
-    const interval = setInterval(fetchEstado, 50000);
+    const interval = setInterval(fetchEstado, 2000);
     return () => clearInterval(interval);
   }, []);
 
@@ -57,13 +59,9 @@ const Luz = ({ dispositivo, alias }) => {
   };
 
   return (
-    <button className="luz">
+    <button className="luz" disabled={estado === null}>
 
-      <div
-        className={buttonClass}
-        onClick={toggleLuz}
-        disabled={estado === null} // Desativa o botão se o estado for desconhecido
-      >
+      <div className={buttonClass} onClick={toggleLuz}>
 
         <div className="btnLuz-display">
 
@@ -74,6 +72,9 @@ const Luz = ({ dispositivo, alias }) => {
             alt="Estado da luz"
             property="true"
           />
+
+          {/* <span className="btnLuz-nome">{dispositivo}</span>
+          <span className="btnLuz-ambiente">{ambiente}</span> */}
 
           <span className="btnLuz-nome">{alias}</span>
 
@@ -89,6 +90,8 @@ const Luz = ({ dispositivo, alias }) => {
             alt="Estado da luz"
             property="true"
           />
+
+          
           
           </div>
         </div>
