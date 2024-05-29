ab636e7fbcfd6303cbabc0aa9ca8cbfde2dd21f8
Blimabru <bru.no@outlook.com.br>
2024-05-29 01:23:03 -0300

    Criação de função que verifica no Banco de Dados a informação referente ao sensor de gás e fumaça

---
+++ src/pages/api/sensores/gasEfumaca.tsx
@@ -0,0 +1,16 @@
+import { query } from '../../../database/db';
+
+// Define o manipulador da API para obter os dados de temperatura do banco de dados
+export default async function handler(req, res) {
+  try {
+
+    const results = await query(
+      'SELECT * FROM sensores WHERE sensor = ? ORDER BY n DESC',
+      ['Gás e Fumaça']
+    );
+    // Retorna os resultados como JSON
+    res.status(200).json(results[0]); // Responde com o primeiro resultado da consulta
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
